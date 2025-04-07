eca1058a2b4bfbcdb2de48b56d3233fd663c474a
Konstantin Mikheev <mik.kon6610@gmail.com>
2025-04-07 15:58:24 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { ButtonMobile } from '@alfalab/core-components/button/mobile';
 
-import { AmountInput } from '@alfalab/core-components/amount-input';
-import { Checkbox } from '@alfalab/core-components/checkbox';
-import { PureCell } from '@alfalab/core-components/pure-cell';
 import { Typography } from '@alfalab/core-components/typography';
 
 import { Gap } from '@alfalab/core-components/gap';
@@ -18,11 +15,6 @@ import { LS, LSKeys } from './ls';
 import { appSt } from './style.css';
 import { ThxLayout } from './thx/ThxLayout';
 
-const creditConstants = {
-  min: 10_000,
-  max: 1_000_000,
-};
-
 export const App = () => {
   const [step, setStep] = useState(1);
   const [loading, setLoading] = useState(false);
@@ -30,21 +22,9 @@ export const App = () => {
   const [address, setAddress] = useState('');
   const [day, setDay] = useState('Сегодня');
   const [time, setTime] = useState('');
-  const [credit, setCredit] = useState(150_000);
   const [isTimeError, setIsTimeError] = useState(false);
   const [details, setDetails] = useState('');
   const [thx, setThx] = useState(LS.getItem(LSKeys.ShowThx, false));
-
-  const handleBlurInputCredit = () => {
-    if (credit < creditConstants.min) {
-      setCredit(creditConstants.min);
-      return;
-    }
-    if (credit > creditConstants.max) {
-      setCredit(creditConstants.max);
-      return;
-    }
-  };
 
   const submit = () => {
     setLoading(true);
@@ -82,10 +62,10 @@ export const App = () => {
             <div style={{ display: 'flex', justifyContent: 'space-around' }}>
               <div>
                 <Typography.Text tag="p" view="primary-medium" defaultMargins={false} weight="medium">
-                  Кэшбэк до 50%
+                  Кэшбэк до 30%
                 </Typography.Text>
                 <Typography.Text tag="p" view="primary-small" color="secondary" defaultMargins={false}>
-                  за покупки у партнёров
+                  Из 6 категорий на выбор
                 </Typography.Text>
               </div>
               <div>
@@ -93,7 +73,7 @@ export const App = () => {
                   Бесплатно
                 </Typography.Text>
                 <Typography.Text tag="p" view="primary-small" color="secondary" defaultMargins={false}>
-                  0 ₽ в первый год
+                  Выпуск и обслуживание
                 </Typography.Text>
               </div>
             </div>
@@ -102,7 +82,7 @@ export const App = () => {
           <Gap size={48} />
 
           <Typography.TitleResponsive font="system" tag="h3" view="small" className={appSt.productsTitle}>
-            Ваши преимущества
+            Как это работает
           </Typography.TitleResponsive>
 
           <Gap size={16} />
@@ -112,10 +92,10 @@ export const App = () => {
               <img src={icon1} alt="" width={48} height={48} style={{ objectFit: 'cover' }} />
               <div style={{ display: 'flex', flexDirection: 'column' }}>
                 <Typography.Text tag="p" view="primary-medium" defaultMargins={false}>
-                  Даем до 1 000 000 ₽
+                  Дебетовая карта
                 </Typography.Text>
                 <Typography.Text tag="p" view="primary-small" color="secondary" defaultMargins={false}>
-                  Тратьте на ежедневные расходы или планируйте крупные покупки
+                  Получайте суперкэшбэк рублями каждый месяц и бесплатно снимайте наличные в любых банкоматах
                 </Typography.Text>
               </div>
             </div>
@@ -123,11 +103,10 @@ export const App = () => {
               <img src={icon2} alt="" width={48} height={48} style={{ objectFit: 'cover' }} />
               <div style={{ display: 'flex', flexDirection: 'column' }}>
                 <Typography.Text tag="p" view="primary-medium" defaultMargins={false}>
-                  60 дней без % на покупки
+                  Категории с повышенным
                 </Typography.Text>
                 <Typography.Text tag="p" view="primary-small" color="secondary" defaultMargins={false}>
-                  Действуют на все покупки по карте. Период без процентов начинает действовать со дня совершения первой
-                  покупки, возобновляется после погашений всей суммы долга
+                  Продукты, маркетплейсы, такси и многое другое. А ещё можно выбрать кэшбэк на всё и получать выгоду всегда
                 </Typography.Text>
               </div>
             </div>
@@ -135,38 +114,16 @@ export const App = () => {
               <img src={icon3} alt="" width={48} height={48} style={{ objectFit: 'cover' }} />
               <div style={{ display: 'flex', flexDirection: 'column' }}>
                 <Typography.Text tag="p" view="primary-medium" defaultMargins={false}>
-                  До 50% кэшбэка за покупки
+                  Крутите барабан суперкэшбэка
                 </Typography.Text>
                 <Typography.Text tag="p" view="primary-small" color="secondary" defaultMargins={false}>
-                  Каждый месяц собираем выгодные предложения, чтобы вы зарабатывали на покупках. Ищите их в разделе
-                  Предложения партнёров
+                  Дадим 40, 60, 80 и даже 100% кэшбэка на случайную категорию или увеличим процент на выбранную категорию
                 </Typography.Text>
               </div>
             </div>
           </div>
 
           <Gap size={24} />
-
-          <Typography.TitleResponsive font="system" tag="h3" view="small" className={appSt.productsTitle}>
-            Выберите кредитный лимит
-          </Typography.TitleResponsive>
-
-          <Gap size={16} />
-
-          <AmountInput
-            value={credit}
-            placeholder="Введите сумму"
-            labelView="outer"
-            label="Кредитный лимит"
-            integersOnly
-            bold={false}
-            block
-            size={48}
-            hint="От 10 000 ₽ до 1 000 000 ₽"
-            onChange={(_, { value }) => setCredit(value ?? 0)}
-            minority={1}
-            onBlur={handleBlurInputCredit}
-          />
         </div>
       )}
 
@@ -187,7 +144,7 @@ export const App = () => {
               })}
             >
               <span>По адресу</span>
-              <span>Через 2 часа</span>
+              <span>Бесплатно</span>
             </SwiperSlide>
             <SwiperSlide
               onClick={() => setDeliveryType('В офис банка')}
@@ -196,64 +153,13 @@ export const App = () => {
               })}
             >
               <span>В офис банка</span>
-              <span>Сегодня</span>
-            </SwiperSlide>
-            <SwiperSlide
-              onClick={() => setDeliveryType('Кредитный счет')}
-              className={appSt.swSlide({
-                selected: deliveryType === 'Кредитный счет',
-              })}
-            >
-              <span>Кредитный счет</span>
-              <span>Моментально</span>
+              <span>Бесплатно</span>
             </SwiperSlide>
           </Swiper>
 
           <Gap size={24} />
 
           <>
-            {deliveryType === 'Кредитный счет' && (
-              <>
-                <Typography.TitleResponsive font="system" tag="h3" view="small" className={appSt.productsTitle}>
-                  Кредитный счет
-                </Typography.TitleResponsive>
-
-                <Gap size={28} />
-
-                <Typography.Text tag="p" view="primary-medium" weight="medium" defaultMargins={false}>
-                  Подключите кредитный лимит к вашей дебетовой карте и пользуйтесь как кредиткой - без процентов на 60 дней
-                </Typography.Text>
-
-                <Gap size={8} />
-
-                <Typography.Text tag="p" view="primary-small" color="secondary" defaultMargins={false}>
-                  Подключите кредитный лимит и вы сможете тратить деньги сразу же - без лишнего пластика. Если вам будет
-                  необходима пластиковая версия карты, вы сможете выпустить ее отдельно.
-                </Typography.Text>
-                <Gap size={28} />
-                <Typography.TitleResponsive font="system" tag="h3" view="small" className={appSt.productsTitle}>
-                  Выберите карту
-                </Typography.TitleResponsive>
-
-                <Gap size={28} />
-
-                <div className={appSt.boxRow}>
-                  <PureCell verticalPadding="none">
-                    <PureCell.Graphics verticalAlign="center">
-                      <img src={image} width={56} height={36} alt="" />
-                    </PureCell.Graphics>
-                    <PureCell.Content>
-                      <PureCell.Main>
-                        <Typography.Text view="primary-medium">Ваша дебетовая Альфа-карта</Typography.Text>
-                      </PureCell.Main>
-                    </PureCell.Content>
-                    <PureCell.Addon verticalAlign="center">
-                      <Checkbox size={'m'} checked className={appSt.checkBox} />
-                    </PureCell.Addon>
-                  </PureCell>
-                </div>
-              </>
-            )}
             {deliveryType === 'По адресу' && (
               <>
                 <Input
@@ -314,17 +220,9 @@ export const App = () => {
                 {day === 'Сегодня' && (
                   <Swiper style={{ marginLeft: '0' }} spaceBetween={8} slidesPerView="auto">
                     <SwiperSlide
-                      onClick={() => setTime('Через 2 часа')}
+                      onClick={() => setTime('Через 4 часа')}
                       className={appSt.swSlideOneLine({
-                        selected: time === 'Через 2 часа',
-                      })}
-                    >
-                      Через 2 часа
-                    </SwiperSlide>
-                    <SwiperSlide
-                      onClick={() => setTime('Через 4 часов')}
-                      className={appSt.swSlideOneLine({
-                        selected: time === 'Через 4 часов',
+                        selected: time === 'Через 4 часа',
                       })}
                     >
                       Через 4 часа
@@ -335,7 +233,15 @@ export const App = () => {
                         selected: time === 'Через 6 часов',
                       })}
                     >
-                      Через 6 часов
+                      Через 6 часа
+                    </SwiperSlide>
+                    <SwiperSlide
+                      onClick={() => setTime('Через 8 часов')}
+                      className={appSt.swSlideOneLine({
+                        selected: time === 'Через 8 часов',
+                      })}
+                    >
+                      Через 8 часов
                     </SwiperSlide>
                   </Swiper>
                 )}
@@ -474,7 +380,7 @@ export const App = () => {
       {step === 1 && (
         <div className={appSt.bottomBtnThx}>
           <ButtonMobile onClick={() => setStep(2)} block view="primary">
-            Продолжить
+            Оформить за 0 ₽
           </ButtonMobile>
         </div>
       )}
@@ -493,7 +399,7 @@ export const App = () => {
             block
             view="primary"
           >
-            Оформить
+            Оформить за 0 ₽
           </ButtonMobile>
         </div>
       )}
